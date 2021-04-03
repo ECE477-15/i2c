51147a54fe1597b731dcffd90bd99afecd3d0f62
grantaweiss <weissg@purdue.edu>
2021-04-03 03:12:35 -0400

    working blocking i2c

--- main.c
+++ main.c
@@ -11,57 +11,83 @@
 #include "stm32l0xx.h"
 #include "stm32l0538_discovery.h"
 
+void I2C_Tx(uint16_t DevAddress, uint8_t *pData, uint16_t Size);
+void I2C_Init();
+
 int main(void)
 {
-	// data to send out to i2c
-	uint8_t data[2] = {0x0, 0b01000000};
+  I2C_Init();
 
-	// Clock setup
-	RCC->IOPENR |= RCC_IOPENR_GPIOBEN;
-	RCC->AHBENR |= RCC_AHBENR_DMA1EN;
+  uint8_t data[2] = {0x3D, 0xFF};
+  I2C_Tx(0x32<<1, data, 2);
 
-	// GPIO Setup
-	GPIOB->MODER = (GPIOB->MODER & ~(GPIO_MODER_MODE10)) | (GPIO_MODER_MODE10_1); // PB10 AF
-	GPIOB->MODER = (GPIOB->MODER & ~(GPIO_MODER_MODE11)) | (GPIO_MODER_MODE11_1); // PB11 AF
+  data[0] = 0x00;
+  data[1] = 0x40;
+  I2C_Tx(0x32<<1, data, 2);
 
-	GPIOB->AFR[1] = (GPIOB->AFR[1] & (0xF << 8)) | (0x6 << 8);		// PB10 AF=6
-	GPIOB->AFR[1] = (GPIOB->AFR[1] & (0xF << 12)) | (0x6 << 12);	// PB11 AF=6
+  data[0] = 0x36;
+  data[1] = 0x53;
+  I2C_Tx(0x32<<1, data, 2);
 
-	// DMA setup
-	DMA1_Channel4->CCR = (DMA1_Channel4->CCR & ~(DMA_CCR_EN | DMA_CCR_MEM2MEM_Msk | DMA_CCR_MSIZE_Msk | DMA_CCR_PSIZE_Msk | DMA_CCR_PINC_Msk | DMA_CCR_CIRC_Msk)) | DMA_CCR_MINC | DMA_CCR_DIR;
-	DMA1_Channel4->CPAR = (uint32_t) (&(I2C2->TXDR));
-	DMA1_Channel4->CMAR = (uint32_t) (&data);
-	DMA1_Channel4->CNDTR = 2;
-	DMA1_Channel4->CCR |= DMA_CCR_EN;
+  data[0] = 0x16;
+  data[1] = 0xFF;
+  I2C_Tx(0x32<<1, data, 2);
 
-	// I2C Setup
-	I2C2->TIMINGR = (uint32_t)0x00300619;
-	I2C2->CR1 |= I2C_CR1_PE;
-	I2C2->CR2 &= ~(I2C_CR2_SADD | I2C_CR2_NBYTES);
-	I2C2->CR2 |= (2<<I2C_CR2_NBYTES_Pos) | (0x32<<1); // address = 0x32
-	I2C2->CR1 |= I2C_CR1_TXDMAEN;
+  int i = 0;
+	while(i < 100000) {
+		i++;
+	}
 
-	// I2C start
-	I2C2->CR2 |= I2C_CR2_START;
+  data[0] = 0x3D;
+  data[1] = 0xFF;
+  I2C_Tx(0x32<<1, data, 2);
 
-	 // Wait for DMA to finish.
-	while ( !( DMA1->ISR & DMA_ISR_TCIF1 ) ) {};
-	DMA1->IFCR |= DMA_IFCR_CTCIF1;
-
-	// Stop the I2C transmission.
-	while ( !( I2C2->ISR & I2C_ISR_TC ) ) {};
-	I2C2->CR2  |=  ( I2C_CR2_STOP );
-	while ( I2C2->ISR & I2C_ISR_BUSY ) {};
-
-	// Data for second transmission
-	data[0] = 0x16;
-	data[1] = 0xFF;
-
-	// I2C start
-	I2C2->CR2 |= I2C_CR2_START; /* Go */
-
-	for(;;);
+  while (1);
 }
 
+void I2C_Tx(uint16_t DevAddress, uint8_t *pData, uint16_t Size) {
+	while(I2C2->ISR & I2C_ISR_BUSY);
+
+	I2C2->CR2 &= ~(I2C_CR2_SADD | I2C_CR2_NBYTES | I2C_CR2_RELOAD | I2C_CR2_AUTOEND | I2C_CR2_RD_WRN | I2C_CR2_START | I2C_CR2_STOP);
+	I2C2->CR2 |= (DevAddress & I2C_CR2_SADD) | (Size << I2C_CR2_NBYTES_Pos) | I2C_CR2_AUTOEND | I2C_CR2_START;
+
+	while(!(I2C2->ISR & I2C_ISR_TXIS));
+	I2C2->TXDR = pData[0];
+
+	while(!(I2C2->ISR & I2C_ISR_TXIS));
+	I2C2->TXDR = pData[1];
+
+	while(!(I2C2->ISR & I2C_ISR_STOPF));
+	I2C2->ICR |= I2C_ICR_STOPCF;
+}
+
+void I2C_Init() {
+    RCC->IOPENR |= RCC_IOPENR_GPIOBEN;	// GPIOB Clock enable
+
+    GPIOB->OSPEEDR |= (0x3 << GPIO_OSPEEDER_OSPEED10_Pos) | (0x3 << GPIO_OSPEEDER_OSPEED11_Pos); // very high speed
+    GPIOB->OTYPER |= GPIO_OTYPER_OT_10 | GPIO_OTYPER_OT_11;	// Open drain
+    GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPD10_Msk | GPIO_PUPDR_PUPD11_Msk);	// clear pullup/pulldown
+    GPIOB->PUPDR |= (GPIO_PUPDR_PUPD10_0 | GPIO_PUPDR_PUPD11_0);		// Set pullup
+	GPIOB->AFR[1] &= ~(GPIO_AFRH_AFSEL10_Msk | GPIO_AFRH_AFSEL11_Msk);					// Clear PB10, PB11 Alternate Function Reg
+	GPIOB->AFR[1] |= (0x6 << GPIO_AFRH_AFSEL10_Pos) | (0x6 << GPIO_AFRH_AFSEL11_Pos);	// Set PB10, PB11 Alternate Function Reg to 0x6 (I2C2)
+	GPIOB->MODER &= ~(GPIO_MODER_MODE10 | GPIO_MODER_MODE11);							// Clear PB10, PB11 mode
+	GPIOB->MODER |= GPIO_MODER_MODE10_1 | GPIO_MODER_MODE11_1;							// Set PB10, PB11 mode to alternate 0b10
+
+    RCC->APB1ENR |= RCC_APB1ENR_I2C2EN;	// I2C2 clock enable
+
+    I2C2->CR1 &= ~I2C_CR1_PE;		// Disable I2C2
+    I2C2->TIMINGR = 0x00707CBB & 0xF0FFFFFFU; // 0xF0FFFFFFU = TIMING_CLEAR_MASK
+
+    I2C2->OAR1 &= ~I2C_OAR1_OA1EN_Msk; 	// Disable Own Address
+    I2C2->OAR1 = (I2C_OAR1_OA1EN | 0x0); // set own address and re-enable
+
+    I2C2->CR2 |= (I2C_CR2_AUTOEND | I2C_CR2_NACK);
+
+    I2C2->OAR2 &= ~I2C_OAR2_OA2EN_Msk;	// Disable own address 2
+
+    I2C2->CR1 = (I2C_GENERALCALL_DISABLE | I2C_NOSTRETCH_DISABLE);
+
+    I2C2->CR1 |= I2C_CR1_PE;		// Enable I2C2
+}
 
 
